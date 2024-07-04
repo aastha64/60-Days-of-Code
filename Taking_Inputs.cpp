51e852e7f2e12d7b29d62741a1dcdccefc1e73e5
aastha64 <113416445+aastha64@users.noreply.github.com>
2024-07-04 16:58:59 +0530

    Add files via upload

---
+++ Taking_Inputs.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // implement cin>> in program
+    int apples;
+    cin>> apples;
+
+    cout<<"Number_of_apples: "<<apples<<endl;
+
+    return 0;
+
+}
