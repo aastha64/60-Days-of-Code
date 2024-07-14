a4e5821c7a0cd8ed557729d1e494bce788d91e02
Aastha <113416445+aastha64@users.noreply.github.com>
2024-07-14 22:47:44 +0530

    Update Sel_Sort.cpp

--- Sel_Sort.cpp
+++ Sel_Sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Selection Sort -  Focuses on finding the minimum element and swap its location with the miminum index 
 void selection_sort(int arr[], int n){
     for(int i = 0; i <= n-2; i++){
-        int min = i;
+        int min = i;                  // We are considering the i = 0 as our first minimum element.
         for(int j = i; j <= n-1; j++){
             if(arr[j] < arr[min]){
                 min = j;
