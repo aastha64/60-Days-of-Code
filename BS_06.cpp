c3b8bcc4c634d4a289d9897d8fdd6031af9b029b
Aastha <113416445+aastha64@users.noreply.github.com>
2024-07-31 16:21:40 +0530

    Add files via upload

---
+++ BS_06.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int TotalBananas(int n, int arr(), int h){
+    int t = 0;
+    int k; int sum = 0;
+    while(h > 0){
+        for(int i = 0; i <= n-1; i++){
+            if(arr[i] <= k){
+                t = 1; 
+            }
+            else {
+                t = arr[i] % k;
+            }
+        }
+        sum += t;
+    }
+
+    return sum;
+}
