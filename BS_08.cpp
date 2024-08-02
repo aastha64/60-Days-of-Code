17a24ff41e93dfdb5ccb24a0ae326ca67209fda7
Aastha <113416445+aastha64@users.noreply.github.com>
2024-08-02 17:50:29 +0530

    Add files via upload

---
+++ BS_08.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int daysReq(vector<int> &arr, int cap){
+    int n = arr.size();
+    int days = 1; 
+    int load = 0;
+    for(int i = 0; i < n; i++){
+        if((load + arr[i]) > cap){
+            days += 1;
+            load = arr[i];
+        }
+        else{
+            load += arr[i];
+        }
+    }
+
+    return days;
+}
+
+int leastWeightCapacity(vector<int> & arr, int d){
+    
+    int maxi = *max_element(arr.begin() , arr.end());
+    int sum = accumulate(arr.begin(), arr.end(), 0);
+
+    for(int i  = maxi; i <= sum; i++){
+
+        if(daysReq(arr, i) <= d){
+            return i;
+        }
+    }
+
+    return -1;
+
+}
+
+int main(){
+    vector<int> arr = {5, 4, 5, 2, 3, 4, 5, 6};
+    int d = 5;
+    int ans = leastWeightCapacity(arr, d);
+    cout << "The minimum capacity should be: " << ans << "\n";
+    return 0;
+
+}
