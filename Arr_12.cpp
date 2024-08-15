977a34ae0a5412d0d85d6b823bfed2f44bef8cd2
Aastha <113416445+aastha64@users.noreply.github.com>
2024-08-15 22:01:59 +0530

    Add files via upload

---
+++ Arr_12.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Find the duplicate in an array of N+1 integers
+
+int findDuplicate(vector < int > & arr){
+    int n = arr.size();
+    int freq[n+1] = {0};
+    for(int i = 0; i < n; i++){
+        if(freq[arr[i]] == 0){
+            freq[arr[i]] += 1;
+        }
+        else{
+            return arr[i];
+        }
+    }
+
+
+    // Optimal Approach 2
+
+    int slow = arr[0];
+    int fast = arr[0];
+
+    do{
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    }while(slow != fast);
+
+    fast = arr[0];
+    while(slow != fast){
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+ return slow;
+
+}
+
+int main() {
+  vector < int > arr;
+  arr = {1,3,4,2,3};
+  cout << "The duplicate element is " << findDuplicate(arr) << endl;
+}
