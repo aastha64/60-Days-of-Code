1063c12dc316033b558c3587c7800c7e8d0f6e39
Aastha <113416445+aastha64@users.noreply.github.com>
2024-08-17 17:27:51 +0530

    Update Arr_14.cpp

--- Arr_14.cpp
+++ Arr_14.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int countPaths(int i,int j,int n,int m,vector<vector<int>> &dp)
     {
-        if(i==(n-1)&&j==(m-1)) return 1;
+        if(i==(n-1)&&j==(m-1)) return 1; // base case
         if(i>=n||j>=m) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
      else return dp[i][j]=countPaths(i+1,j,n,m,dp)+countPaths(i,j+1,n,m,dp);
