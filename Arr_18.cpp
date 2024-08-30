738649e370d897aa3cb5d1dc09d9ae3e836df7e9
Aastha <113416445+aastha64@users.noreply.github.com>
2024-08-30 22:13:26 +0530

    Update Arr_18.cpp

--- Arr_18.cpp
+++ Arr_18.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+//Given two integers a and b, return the sum of the two integers without using the operators + and -.
+class Solution {
+public:
+    int getSum(int a, int b) {
+       while(b!=0){
+            int carry=a&b;
+            a=a^b;
+            b=carry<<=1;
+        }
+        return a; 
+    }
+};
